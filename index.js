88f912295f5d5fb6b130366b3e8eaa5cb3a738e1
Markus Olsen <molsen13@gmail.com>
2015-06-25 09:27:00 -0600

    Sending song url info to rails app

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const redis = require('redis');
 const clientSubscriber = redis.createClient();
 const clientPublisher = redis.createClient();
+const songSubscriber = redis.createClient();
 
 const app = express();
 const server = require('http').createServer(app);
@@ -12,6 +13,8 @@ server.listen(4200);
 
 const mainSocket = io.of('/main');
 const rockSocket = io.of('/genres/rock');
+
+const https = require('https');
 
 var connections = 0;
 
@@ -24,7 +27,6 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function () {
     console.log('Client disconnected');
     socket.disconnect();
-    connections--;
   });
 });
 
@@ -39,6 +41,7 @@ mainSocket.on('connection', function (socket) {
 
 clientSubscriber.subscribe('main');
 clientSubscriber.subscribe('rock');
+clientSubscriber.subscribe('main-song');
 
 clientSubscriber.on('message', function (channel, message) {
   console.log(message);
@@ -50,13 +53,55 @@ clientSubscriber.on('message', function (channel, message) {
 function emitMessages (message, channel) {
   channel = prependGenresToChannelName(channel);
   var clientChannel = io.of('/' + channel);
+  console.log(clientChannel);
   clientChannel.emit('message', message);
 };
 
 function prependGenresToChannelName (channel) {
-  if (channel === 'main') {
-    return 'main';
+  if (channel.indexOf('main') !== -1 || channel.indexOf('song') !== -1) {
+    return channel;
   } else {
     return 'genres/' + channel;
   }
 };
+
+var clientId = '4cf0c14d2d5b74c42671e0371bbf1ea8';
+var url = "https://api.soundcloud.com/tracks.json?client_id=" + clientId;
+var defaultUrl = 'https://soundcloud.com/litanymusic/woman-ft-appleby';
+
+function fetchSong () {
+  https.get(url, function (res) {
+    var body = '';
+    var songs;
+    var randomSong;
+    var songDuration;
+    var songUrl;
+
+    res.on('data', function (chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function () {
+      songs = JSON.parse(body);
+      randomSong = songs[Math.floor(Math.random()*songs.length)];
+      songDuration = randomSong.duration;
+      songUrl = randomSong.permalink_url;
+      console.log('random song: ', randomSong);
+      console.log('random song duration: ', songDuration);
+      console.log('random song permalink: ', songUrl);
+      emitSong(songUrl);
+      //setInterval(fetchSong, songDuration);
+      //console.log(songUrl);
+    });
+
+  }).on('error', function (e) {
+    console.log('got error: ', e);
+  });
+
+};
+
+function emitSong (songUrl) {
+  songSubscriber.publish('main-song', JSON.stringify(songUrl));
+};
+
+fetchSong();
